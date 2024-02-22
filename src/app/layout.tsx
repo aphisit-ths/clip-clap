6cec581f129207ad85c59df5b35d42bf552494c3
oath aphisit <aphisit.thu@turbo.co.th>
2024-02-22 02:45:40 +0000

    chore : title.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "ConClap",
+    title: "Clip-Clap",
     description: "Easy Connection string !",
 };
 
